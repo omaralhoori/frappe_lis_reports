ed90873b5e9b26c0492e0f28971167e7629d4256
omar alhori <omaralhoori@gmail.com>
2024-07-08 01:02:47 +0300

    create templte

---
+++ lis_reports/lis_reports/doctype/medical_examination_template/test_medical_examination_template.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024, omar and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestMedicalExaminationTemplate(unittest.TestCase):
+	pass
---
+++ lis_reports/lis_reports/doctype/special_medical_tests/special_medical_tests.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024, omar and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class special_medical_tests(Document):
+	pass
